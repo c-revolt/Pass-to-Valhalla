5c4663f22b3ad1db279c12a584b141b0214b0c26
Alexander <a.pride.xxi@tutanota.com>
2022-07-12 11:09:46 +0300

    initial commit

---
+++ Pass to Valhalla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pass to Valhalla
+//
+//  Created by Александр Прайд on 12.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .darkGray
+    }
+
+
+}
+
